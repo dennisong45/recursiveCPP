8142cf6b68c1950a4fda77c11cd3301c558bb954
dennisong45 <dennisongwenghan@gmail.com>
2021-08-04 19:03:51 +0000

    reverse String recursive

---
+++ revString.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Problem reverse a string Recursively
+//
+
+string revStr(string input){
+  if(input.size() == 0){
+    return input;
+  }
+
+ 
+  // hello    
+
+  // ello
+  // llo
+  // ll
+  // l
+  //
+  return revStr(input.substr(1)) + "H";
+}
+
+
+int main() {
+  
+  cout << revStr("hello");
+}
